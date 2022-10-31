c5a8bea40ab7f2b4d65560c9b500b5e6d38b05fa
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-10-31 09:36:15 +0530

    MongoDB Connection created

---
+++ sensor/configurations/mongo_db_connection.py
@@ -0,0 +1,21 @@
+import pymongo
+from sensor.constants.database import DATABASE_NAME
+from sensor.constants.env_variables import MONGODB_URL_KEY
+
+import certifi
+import os
+ca = certifi.where()
+
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                MongoDBClient.client = pymongo.MongoDBClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
