aa9378e58089ab7e80e411f293fff9219e851064
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-11-01 09:46:44 +0530

    Updated training Pipeline

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,1 +1,21 @@
-from sensor.entity.config_entity import
+from sensor.entity.config_entity import TrainingPipelineConfig
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os, sys
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.training_pipeline_config = TrainingPipelineConfig()
+
+    def  start_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            rasie SensorException(e, sys)
+
+    def run_pipeline(self):
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys)
