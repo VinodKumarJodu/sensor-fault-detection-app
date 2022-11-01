69b83c5503d2fc848157651e066fb0c957e2b887
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-11-01 09:01:12 +0530

    Updated main_utils.py

---
+++ sensor/utils/main_utils.py
@@ -0,0 +1,14 @@
+import yaml
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os, sys
+import numpy as np 
+import dill
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path,"rb") as yaml_file:
+            return yaml.safeload(yaml_file)
+    except Exception as e:
+        raise SensorException(e,sys)
