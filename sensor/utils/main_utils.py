03ae0ec20a28744c5b9ef1c05ca70440c00c1a9c
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-11-02 22:15:48 +0530

    Written write_yaml_file to write report

--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -1,9 +1,9 @@
-import yaml
-from sensor.exceptions import SensorException
-from sensor.logger import logging
 import os, sys
-import numpy as np 
+import yaml
 import dill
+import numpy as np 
+from sensor.logger import logging
+from sensor.exceptions import SensorException
 
 
 def read_yaml_file(file_path: str) -> dict:
@@ -12,3 +12,15 @@ def read_yaml_file(file_path: str) -> dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise SensorException(e,sys)
+
+def write_yaml_file(file_path: str, content: object, replace: bool=False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path, exist_ok=True))
+        with open(file_path,"w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise SensorException(e, sys)
+
